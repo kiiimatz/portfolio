b361a539345f53be0657b387c17bab432b6ac315
kiiimatz <kiiimatsy@gmail.com>
2026-05-02 23:02:13 +0900

    ttf to woff2, update fontux

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,8 @@
 import { createHandle } from "@kiiimatz/fontux/hooks";
 
 export const handle = createHandle([
-  { class: "writer", font: "/fonts/writer-regular.ttf" },
-	  { class: "writer-mx", font: "/fonts/writer-medium.ttf" },
-	  { class: "sans", font: "/fonts/googlesans-medium.ttf" },
-	  { class: "sans-bx", font: "/fonts/googlesans-bold.ttf" },
+  { class: "writer", font: "/fonts/writer-regular.woff2" },
+	  { class: "writer-mx", font: "/fonts/writer-medium.woff2" },
+	  { class: "sans", font: "/fonts/googlesans-medium.woff2" },
+	  { class: "sans-bx", font: "/fonts/googlesans-bold.woff2" },
 ]);
